a17b8a4911e0070c34830b8643adc5e0f389c324
Joey <jjiemjitpolchai9540@bths.edu>
2019-01-08 12:15:06 -0500

    create schema for todo items

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require("express");
+var MongoClient = require('mongodb').MongoClient;
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/todo", {useNewUrlParser: true});
+var url = "mongodb://localhost:27017/todo";
 var app = express();
+
+var Schema = mongoose.Schema;
+var todoSchema = new mongoose.Schema({
+    title: String    
+});
+var Todos = mongoose.model("Todos", todoSchema);
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -7,6 +18,11 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
+MongoClient.connect(url, {useNewUrlParser: true},function(err, db){
+    if(err) throw err;
+    console.log("Db created");
+    db.close();
+});
 
 app.listen(8080, function(){
     console.log("Server is listening");
