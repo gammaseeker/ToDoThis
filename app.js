f2792bd042a7dab296c30d8bd41e7d0acaa61093
Joey <jjiemjitpolchai9540@bths.edu>
2019-01-09 10:37:08 -0500

    form is working with post req

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var MongoClient = require('mongodb').MongoClient;
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/todo", {useNewUrlParser: true});
+var bodyParser = require('body-parser');
 var url = "mongodb://localhost:27017/todo";
 var app = express();
 
@@ -12,10 +13,18 @@ var todoSchema = new mongoose.Schema({
 var Todos = mongoose.model("Todos", todoSchema);
 
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("index");
+});
+
+app.post("/submit", function(req, res){
+    console.log(req.body.todo);
+    res.redirect("/");
 });
 
 MongoClient.connect(url, {useNewUrlParser: true},function(err, db){
