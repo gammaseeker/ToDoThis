420e3d47ab24f611ee5fcf97071888259314b1e0
gammaseeker <joeyjiem@gmail.com>
2021-04-26 14:41:09 -0400

    established mongodb connection

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.get('/api/get_all_todo_items', (req, res) => {
+router.get('/', function (req, res) {
+res.send('Hello World!');
+});
+
+router.get('/api/get_all_todo_items', (req: Request, res: Response) => {
     return res.send('get api working');
 });
-router.post('/api/create_todo_item', (req, res) => {
+
+router.post('/api/create_todo_item', (req: Request, res: Response) => {
     return res.send('post api working');
 });
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,20 @@
-import express = require('express');
+import express from 'express'; 
+import mongoose from 'mongoose';
 import { json } from 'body-parser';
-
-let mongoose = require('mongoose');
+import { todoRouter } from './routes/index';
 
 // Create a new express app instance
 const app: express.Application = express();
 
 app.use(json());
+app.use(todoRouter);
 
-app.get('/', function (req, res) {
-res.send('Hello World!');
+mongoose.connect('mongodb://localhost:27017/todo', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log('connected to db');
 });
 
 app.listen(3001, function () {
