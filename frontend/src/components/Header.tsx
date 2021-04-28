5483ed93d1147083297a37b2b795e8851f113631
gammaseeker <joeyjiem@gmail.com>
2021-04-28 11:24:21 -0400

    init to do list components

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="App-header">
+            <h1>ToDoThis</h1>
+            <h2>A to do list app by Joey Jiemjitpolchai</h2>
+        </header>
+    );
+}
+
+export default Header;
