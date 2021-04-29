a4c641dba1fdd4a1b0f1f00ca1f09880d362ce8b
gammaseeker <joeyjiem@gmail.com>
2021-04-29 16:14:40 -0400

    drafted component tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
 import Header from "./components/Header";
+import Card from "./components/UI/Card";
+import TodoItem from "./components/TodoItem";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Card className="expenses">
+        <TodoItem />
+      </Card>
     </div>
   );
 }
--- frontend/src/components/TodoItem.tsx
+++ frontend/src/components/TodoItem.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import Card from './UI/Card';
+import React from "react";
+import Card from "./UI/Card";
+import "./TodoItem.css";
 
 const TodoItem = () => {
-
-}
+    return (
+  <Card className="expense-item">
+    <div className="expense-item__description">
+        asdf
+    </div>
+  </Card>);
+};
 
 export default TodoItem;
