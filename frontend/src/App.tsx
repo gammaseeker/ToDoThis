d7c690d5b88894ee15b8690db44a1b297d9304d8
gammaseeker <joeyjiem@gmail.com>
2021-05-03 14:10:32 -0400

    update imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
-import Header from "./components/Header";
+import Header from "./components/UI/Header";
 import Card from "./components/UI/Card";
-import TodoItem from "./components/TodoItem";
+import TodoItem from "./components/Todos/TodoItem";
 import "./App.css";
 
 const App = () => {
